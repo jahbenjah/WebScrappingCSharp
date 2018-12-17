ef323468f75369dae7a1b9d76686ef32af7a87ef
Benjamin Camacho <benjamin.camacho@dabarc.com>
2018-12-17 15:07:19 -0600

    Se crea estructura de proyecto y se agregan referencias

---
+++ src/AppCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
