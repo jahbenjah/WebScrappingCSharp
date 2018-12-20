925df03ec1155a499d5dc968c6005d2fa2b609e2
Benjamin Camacho <benjamin.camacho@dabarc.com>
2018-12-20 16:31:21 -0600

    Se crea modelo y se agregan migraciones

---
+++ src/AppData/AppContext.cs
@@ -0,0 +1,30 @@
+﻿using AppCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppData
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Libro> Libros { get; set; }
+
+        public DbSet<Libro> Categorias { get; set; }
+
+        public AppContext()
+        {
+
+        }
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Libros.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/AppData/Migrations/20181220222434_Inicial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppData.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(nullable: true),
+                    UrlImage = table.Column<string>(nullable: true),
+                    Titulo = table.Column<string>(nullable: true),
+                    Precio = table.Column<decimal>(nullable: false),
+                    CategoriaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libro", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Libro_Categoria_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categoria",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libro_CategoriaId",
+                table: "Libro",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libro");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
